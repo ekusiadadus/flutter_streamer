5efcc3d730dd897e11a889fceb1beb78d44994a7
ekusiadadus <ekusiadadus@gmail.com>
2022-03-16 22:35:13 +0900

    add: HomePage Logo

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _channelName = TextEditingController();
+  final _userName = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +19,47 @@ class _HomeState extends State<Home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(),
+            Image.asset("assets/streamer.png"),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text("Multi Streaming with Friends"),
+            const SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: TextField(
+                controller: _userName,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  hintText: 'User Name',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: TextField(
+                controller: _channelName,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  hintText: 'Channel Name',
+                ),
+              ),
+            ),
             TextField(),
             TextButton(
               onPressed: () {
